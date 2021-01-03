fc2ebc56136f4e213b4216e35f770aeff4d0ff24
jh <john.hardy@workjam.com>
2021-01-04 00:08:48 +1100

    format

--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,4 @@ import { CellType } from './memory';
 export const asBool = (b: boolean): number => (b ? -1 : 0);
 
 export const formatCell = (value: number, cellType: CellType): string =>
-    cellType === CellType.float ? value.toFixed(2).replace(/\.?0*$/, '') : value.toString();
+    cellType === CellType.float ? value.toFixed(2).replace(/0*$/, '') : value.toString();
